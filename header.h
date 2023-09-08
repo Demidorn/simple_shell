5d019d791c2934dda5d9c2e9cc97dd651f437961
Demidorn <dornkaizen@gmail.com>
2023-09-08 05:39:32 -0700

    header files and prototypes to the simple shell

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <strings.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[], char **env);
+
+#endif
