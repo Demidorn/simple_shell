64b4d5048e93bad6f16048622d1a22d620910d4d
Demidorn <dornkaizen@gmail.com>
2023-09-20 09:28:37 -0700

    envs

--- DL_getenv.c
+++ DL_getenv.c
@@ -4,19 +4,55 @@
  * _getenv - gets an env variable from the path
  * @str: name of the variable to get
  *
- * Return: pointer to the env variable else null
+ * Return: value of the env variable else null
  */
 
-char **_getenv(const char *str)
+char *_getenv(const char *str)
 {
-	int lens, j;
+	size_t len;
+	char *value;
+	int l;
 
-	lens = _strlen((char *)str);
+	len = _strlen(str);
 
-	for (j = 0; environ[j]; j++)
+	if (str == NULL || environ == NULL)
 	{
-		if (_strncmp(str, environ[j], lens) == 0)
-			return (&environ[j]);
+		perror("Error: ");
+		return (NULL);
+	}
+
+	for (l = 0; environ[l]; l++)
+	{
+		if (_strncmp(str, environ[l], len) == 0 && environ[l][len] == '=')
+		{
+			value = _strdup(environ[l] + len + 1);
+			if (value == NULL)
+			{
+				perror("Error: ");
+				return (NULL);
+			}
+			return (value);
+		}
 	}
 	return (NULL);
+}
+
+/**
+ * read_input - function that reads a line of input
+ * @get_address: poniter to store input
+ * @length_input: stores length of the input line
+ *
+ * Return: number of characters read otherwise -1 on failure
+ */
+
+ssize_t read_input(char **get_address, size_t *length_input)
+{
+	char *prompt = "$ ";
+	ssize_t read_line;
+	int is_terminal = isatty(0);
+
+	if (is_terminal == 1)
+		write(STDOUT_FILENO, prompt, 2);
+	read_line = getline(get_address, length_input, stdin);
+	return (read_line);
 }
