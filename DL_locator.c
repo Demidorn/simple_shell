10a136af52b067d6e982fe0867f8fd3205a9120a
Demidorn <dornkaizen@gmail.com>
2023-09-20 09:29:51 -0700

    locstes path

--- DL_locator.c
+++ DL_locator.c
@@ -15,28 +15,28 @@ char *locate_path(char *com_val)
 	struct stat buff;
 
 	if (stat(com_val, &buff) == 0)
-		return (strdup(com_val));
-	read_path = getenv("PATH");
+		return (_strdup(com_val));
+	read_path = _getenv("PATH");
 	if (!read_path)
 		return (NULL);
-	copy_of_path = strdup(read_path);
+	copy_of_path = _strdup(read_path);
 	if (!copy_of_path)
 		return (NULL);
-	token_to_path = strtok(copy_of_path, ";");
+	token_to_path = strtok(copy_of_path, ":");
 
 	while (token_to_path != NULL)
 	{
-		dir_length = strlen(tken_to_path);
-		length = strlen(com_val);
-		file = malloc(length + dir_length + 2);
+		dir_length = _strlen(token_to_path);
+		length = _strlen(com_val);
+		file = malloc(dir_length + length + 2);
 		if (!file)
 		{
 			free(copy_of_path);
 			return (NULL);
 		}
-		strcpy(file, token_to_path);
-		strcat(file, "/");
-		strcat(file, com_val);
+		_strcpy(file, token_to_path);
+		_strcat(file, "/");
+		_strcat(file, com_val);
 		if (stat(file, &buff) == 0)
 		{
 			free(copy_of_path);
